db83cd3ab00697f28b2def2cebcdef52698fdd92
suet-lee <styinglee@gmail.com>
2021-06-23 17:25:28 +0100

    Update dronekit_conn

--- mycelium_utils/dronekit_conn.py
+++ mycelium_utils/dronekit_conn.py
@@ -68,6 +68,53 @@ class DronekitConnector:
             return [gps.lat, gps.lon]
         return [None, None]
 
+    def upload_mission(self, waypoints_file):
+        """
+        Upload a mission from a file.
+        """
+        #Read mission from file
+        missionlist = self._readmission(waypoints_file)
+
+        #Clear existing mission from vehicle
+        cmds = self.conn.commands
+        cmds.clear()
+        #Add new mission to vehicle
+        for command in missionlist:
+            cmds.add(command)
+            
+        self.conn.commands.upload()
+
+    def _readmission(self, file_name):
+        """
+        Load a mission from a file into a list.
+
+        This function is used by upload_mission().
+        """
+        cmds = self.conn.commands
+        missionlist=[]
+        with open(file_name) as f:
+            for i, line in enumerate(f):
+                if i==0:
+                    if not line.startswith('QGC WPL 110'):
+                        raise Exception('File is not supported WP version')
+                else:
+                    linearray=line.split('\t')
+                    ln_index=int(linearray[0])
+                    ln_currentwp=int(linearray[1])
+                    ln_frame=int(linearray[2])
+                    ln_command=int(linearray[3])
+                    ln_param1=float(linearray[4])
+                    ln_param2=float(linearray[5])
+                    ln_param3=float(linearray[6])
+                    ln_param4=float(linearray[7])
+                    ln_param5=float(linearray[8])
+                    ln_param6=float(linearray[9])
+                    ln_param7=float(linearray[10])
+                    ln_autocontinue=int(linearray[11].strip())
+                    cmd = dronekit.Command( 0, 0, 0, ln_frame, ln_command, ln_currentwp, ln_autocontinue, ln_param1, ln_param2, ln_param3, ln_param4, ln_param5, ln_param6, ln_param7)
+                    missionlist.append(cmd)
+        return missionlist
+
     def disconnect(self):
         self.conn.close()
 
